0a7845e57da1b073ed2e930951436ff8c5e3ad32
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-06-28 17:47:53 +0100

    max of four
    write function which reads four arguments and return the greatest of them

---
+++ C Language/Easy/FunctionInC.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max(int x, int y) {
+    if (x > y){
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int max_of_four(int a, int b, int c, int d){
+    int maximum;
+    maximum = max(max(max(a, b), c), d);
+    return maximum;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+
+
